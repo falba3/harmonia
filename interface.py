7698a1cc07ccef5127e1aeb65e3e3d1f2f197e18
falba3 <99144557+falba3@users.noreply.github.com>
2023-09-29 15:16:46 +0200

    0. Setup

---
+++ interface.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+limit = 100
+with open('requests_log.txt', 'r') as file:
+    n_reqs = int(file.readline())
+
+
+st.title("Interface")
+st.subheader(f"You have {n_reqs} requests so far")
+st.subheader(f"You have {limit - n_reqs} requests remaining")
+
+
+r = st.button("Request:")
+if r:
+    st.text("You requested")
+    with open('requests_log.txt', 'w') as file:
+        # new_reqs = n_reqs + 1
+        # print(file, new_reqs)
+        pass
+
