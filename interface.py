57e28d428e4363bd0755d954b5d4d0a54d1ffce4
falba3 <99144557+falba3@users.noreply.github.com>
2023-09-29 15:48:55 +0200

    0. Setup

--- interface.py
+++ interface.py
@@ -5,7 +5,7 @@ def interface():
     st.title("Interface")
     r = st.button("Request:")
     if r:
-        with open('requests_log.txt', 'r') as file:
+        with open('requests_log.txt', 'r+') as file:
             n_reqs = int(file.readline())
         with open('requests_log.txt', 'w') as file:
             file.write(str(n_reqs + 1))
@@ -13,7 +13,5 @@ def interface():
             n_reqs = int(file.readline())
         st.subheader(f"You have {n_reqs} requests so far")
         st.subheader(f"You have {limit - n_reqs} requests remaining")
-
-        st.text("You requested")
 
 interface()
